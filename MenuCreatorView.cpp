ecec2fe1af6c0704ccb4e142efe9435991d959c1
sikosis <sikosis>
2003-12-28 11:16:53 +0000

    More work on the MenuCreator class.

---
+++ MenuCreatorView.cpp
@@ -0,0 +1,42 @@
+/*
+
+MenuCreatorView by Sikosis
+
+Released under the MIT license.
+
+(C) 2003 http://brie.sf.net/
+
+*/
+
+// Includes -------------------------------------------------------------------------------------------------- //
+//#include <Alert.h>
+#include <Application.h>
+//#include <Button.h>
+//#include <Entry.h>
+//#include <File.h>
+//#include <FilePanel.h>
+//#include <ListView.h>
+//#include <Path.h>
+//#include <Screen.h>
+//#include <ScrollView.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <TextControl.h>
+#include <Window.h>
+#include <View.h>
+
+#include "brie.h"
+#include "BRIEWindows.h"
+#include "BRIEViews.h"
+#include "brieconstants.h"
+// ------------------------------------------------------------------------------------------------------------- //
+
+
+// MenuCreatorView - Constructor
+MenuCreatorView::MenuCreatorView (BRect frame) : BView (frame, "MenuCreatorView", B_FOLLOW_ALL_SIDES, B_WILL_DRAW )
+{
+	SetViewColor(ui_color(B_PANEL_BACKGROUND_COLOR)); // Set the Background Color
+}
+// ---------------------------------------------------------------------------------------------------------- //
+
+
